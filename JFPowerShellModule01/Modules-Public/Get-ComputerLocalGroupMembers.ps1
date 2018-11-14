0d1c926117859ada9bd9fed0f86cf5f86cbfc918
Felpel <jerefelp@donegalgroup.com>
2018-11-14 09:29:13 -0500

    added additional details to the help file of Get-ComputerLocalGroupMembers.ps1

--- JFPowerShellModule01/Modules-Public/Get-ComputerLocalGroupMembers.ps1
+++ JFPowerShellModule01/Modules-Public/Get-ComputerLocalGroupMembers.ps1
@@ -1,17 +1,34 @@
-<#
-
-.Example
-Get-ComputerLocalGroupMembers -ComputerName computername
-This will get all members of all local groups from the specified computer
-
-.Example
-Get-ADComputer -Filter {name -like '*name*'} | select -ExpandProperty name | sort name | % {Get-ComputerLocalGroupMembers -ComputerName $_} | Export-Csv export.csv
-This will get a list of computers from AD that match the filter, Select the "name" property from the results, sort the names, then run "Get-ComputerLocalGroupMembers"
-against each result. Then finally dump the whole output to a csv. 
-
-#>
 function Get-ComputerLocalGroupMembers {
-	[CmdletBinding()]
+	<#
+	
+	.Synopsis
+	Get a list of all local group members from the specified computer.
+	
+	.Description
+	The Get-ComputerLocalGroupMembers function is used to retreive a list of local group members from a specified computer.
+	One return entry will result for each member of each group. Meaning, if a user is a member of three groups, three entries will be returned.
+
+	Also, for this function to be able to gather WMI data from the remote computer, the host running the script will need to have admin privledges to that remote computer.
+	
+	.NOTES
+	Written by Jeremy Felpel
+	GitHub: jfelpel
+	
+	.LINK
+	https://github.com/jfelpel/JF-PowerShell01
+	
+	.Example
+	Get-ComputerLocalGroupMembers -ComputerName computername
+	This will get all members of all local groups from the specified computer
+	
+	.Example
+	Get-ADComputer -Filter {name -like '*name*'} | select -ExpandProperty name | sort name | % {Get-ComputerLocalGroupMembers -ComputerName $_} | Export-Csv export.csv
+	This will get a list of computers from AD that match the filter, Select the "name" property from the results, sort the names, then run "Get-ComputerLocalGroupMembers"
+	against each result. Then finally dump the whole output to a csv.
+
+	#>
+
+	# [CmdletBinding()]
 	param (
 		[parameter(Mandatory=$true)]
 		[String]
@@ -50,5 +67,3 @@ function Get-ComputerLocalGroupMembers {
 	#print the list of groups and group members
 	$ListofGroupsandMembers
 }
-
-# Get-ComputerLocalGroupMembers
